05589b57dfd239a7e8a2ac65e0a94a322647c83a
lrmoon <larariad@rocketmail.com>
2023-03-07 21:46:31 -0500

    render

--- notaryservice/settings.py
+++ notaryservice/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.vivianrashidi.com','notaryservice.herokuapp.com', 'vivianrashidi.com']
+ALLOWED_HOSTS = ['*','www.vivianrashidi.com','notaryservice.herokuapp.com', 'vivianrashidi.com']
 
 
 # Application definition
