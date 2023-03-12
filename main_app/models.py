5bffa78bdda0b031f594c934ea518bf69f960d17
lrmoon <larariad@rocketmail.com>
2023-03-12 00:22:56 -0500

    xml changes

--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,3 @@
-from django.urls import reverse
-from django.utils import timezone
+
 from django.db import models
 
-class MyModel(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-
-    def get_absolute_url(self):
-        return reverse("main_app:home", kwargs={"slug": self.slug})
-    
-    def get_lastmod(self):
-        return timezone.now()
-    
