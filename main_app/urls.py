cc34f897fb8a4bf7fc4efcd10da87f6075f34679
lrmoon <larariad@rocketmail.com>
2023-03-12 20:58:24 -0400

    changed sitemap location

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
-from django.contrib.sitemaps.views import sitemap
-from .sitemap import StaticViewsSitemap
-
-
-
 from . import views
 
 app_name = 'main_app'
 
-sitemaps = {
-    'sitemap': StaticViewsSitemap,
-}
-
-
 urlpatterns = [
     path('', views.home, name='home'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'), #this is straight from docs
+    
     
 ] 
